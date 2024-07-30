0a13e1e26a21dd2011296c3d256ff3d2c3b4ec2b
wzseba <wzseba@gmail.com>
2024-07-29 22:52:13 -0300

    add: estructura de datos en progreso

--- src/main/SistemaTickets.java
+++ src/main/SistemaTickets.java
@@ -1,8 +1,14 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SistemaTickets {
 
 	public static void main(String[] args) {
+		
+		// Estructura de datos
+		List<Tickets> listaTickets = new ArrayList<Tickets>();
 		
 	}
 }
