3934741008ee3c1b6f5f217efd1d9c3a06b2b1d9
wzseba <wzseba@gmail.com>
2024-07-31 22:55:34 -0300

    add: creacion y almacenamiento de tickets

--- src/main/SistemaTickets.java
+++ src/main/SistemaTickets.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class SistemaTickets {
@@ -9,6 +10,25 @@ public class SistemaTickets {
 		
 		// Estructura de datos
 		List<Tickets> listaTickets = new ArrayList<Tickets>();
+		
+		// Creacion de Ticket mediante el constructor con parametros
+		Tickets ticket1 = new Tickets(2,2,2,new Date(),new Date(),2150);
+		Tickets ticket2 = new Tickets(3,2,3,new Date(),new Date(),1150);
+		
+		// Creacion de Ticket usando constructor vacio. Se aplica encapsulamiento
+		Tickets ticket3 = new Tickets();
+		ticket3.setAsiento(4);
+		ticket3.setFechaCompra(new Date());
+		ticket3.setFechaValidez(new Date());
+		ticket3.setFila(4);
+		ticket3.setNumero(8);
+		ticket3.setPrecio(1200);
+		
+		//Almacenar numero indeterminado de tickets
+		listaTickets.add(ticket1);
+		listaTickets.add(ticket2);
+		listaTickets.add(ticket3);
+		
 		
 	}
 }
