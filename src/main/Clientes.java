355fb8985054be564383d9428b99740ba8b3e481
wzseba <wzseba@gmail.com>
2024-08-04 01:19:07 -0300

    add: plantilla de clientes

---
+++ src/main/Clientes.java
@@ -0,0 +1,46 @@
+package main;
+
+public class Clientes {
+	
+	 private int id;
+	 private String nombre;
+	 private String apellido;
+	 
+	 public Clientes() {}
+
+	 public Clientes(int id, String nombre, String apellido) {
+		 this.id = id;
+		 this.nombre = nombre;
+		 this.apellido = apellido;
+	 }
+	 
+	 public int getId() {
+		 return id;
+	 }
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return " [ id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + " ]";
+	}
+
+}
