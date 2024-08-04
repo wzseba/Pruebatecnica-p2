d3176ec0ca6bf514418947962c567f135cb3472c
wzseba <wzseba@gmail.com>
2024-08-04 01:20:58 -0300

    add: creacion de clientes para tickets

--- src/main/SistemaTickets.java
+++ src/main/SistemaTickets.java
@@ -12,9 +12,14 @@ public class SistemaTickets {
 		// Estructura de datos
 		List<Tickets> listaTickets = new ArrayList<Tickets>();
 		
+		// Creacion de Clientes
+		Clientes cliente1 = new Clientes(85,"sebastian","vera");
+		Clientes cliente2 = new Clientes(20,"daniela","rodriguez");
+		Clientes cliente3 = new Clientes(8,"jose","lopez");
+		
 		// Creacion de Ticket mediante el constructor con parametros
-		Tickets ticket1 = new Tickets(2,2,2,new Date(),new Date(),2150);
-		Tickets ticket2 = new Tickets(3,2,3,new Date(),new Date(),1150);
+		Tickets ticket1 = new Tickets(2,2,2,new Date(),new Date(),2150,cliente1);
+		Tickets ticket2 = new Tickets(3,2,3,new Date(),new Date(),1150,cliente2);
 		
 		// Creacion de Ticket usando constructor vacio. Se aplica encapsulamiento
 		Tickets ticket3 = new Tickets();
@@ -24,6 +29,7 @@ public class SistemaTickets {
 		ticket3.setFila(4);
 		ticket3.setNumero(8);
 		ticket3.setPrecio(1200);
+		ticket3.setCliente(cliente3);
 		
 		//Almacenar numero indeterminado de tickets
 		listaTickets.add(ticket1);
@@ -31,7 +37,11 @@ public class SistemaTickets {
 		listaTickets.add(ticket3);
 		
 		// Suma de precio de todos los tickets
-		
+		double total= 0;
+		for (Tickets tick : listaTickets) {
+			total += tick.getPrecio();
+		}
+		System.out.println("Total: $" + total);
 		
 		// Solicitar al usuario que ingrese un numero de fila
 		Scanner input = new Scanner(System.in);
