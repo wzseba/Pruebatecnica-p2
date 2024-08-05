432f28eff2e6fd99aa9ca0b545054adcc5c272f0
wzseba <wzseba@gmail.com>
2024-08-04 22:18:16 -0300

    refactor: creacion de cliente

--- src/main/SistemaTickets.java
+++ src/main/SistemaTickets.java
@@ -12,14 +12,10 @@ public class SistemaTickets {
 		// Estructura de datos
 		List<Tickets> listaTickets = new ArrayList<Tickets>();
 		
-		// Creacion de Clientes
-		Clientes cliente1 = new Clientes(85,"sebastian","vera");
-		Clientes cliente2 = new Clientes(20,"daniela","rodriguez");
-		Clientes cliente3 = new Clientes(8,"jose","lopez");
 		
 		// Creacion de Ticket mediante el constructor con parametros
-		Tickets ticket1 = new Tickets(2,2,2,new Date(),new Date(),2150,cliente1);
-		Tickets ticket2 = new Tickets(3,2,3,new Date(),new Date(),1150,cliente2);
+		Tickets ticket1 = new Tickets(1,2,2,new Date(),new Date(),2150);
+		Tickets ticket2 = new Tickets(2,2,3,new Date(),new Date(),1150);
 		
 		// Creacion de Ticket usando constructor vacio. Se aplica encapsulamiento
 		Tickets ticket3 = new Tickets();
@@ -27,9 +23,8 @@ public class SistemaTickets {
 		ticket3.setFechaCompra(new Date());
 		ticket3.setFechaValidez(new Date());
 		ticket3.setFila(4);
-		ticket3.setNumero(8);
+		ticket3.setNumero(3);
 		ticket3.setPrecio(1200);
-		ticket3.setCliente(cliente3);
 		
 		//Almacenar numero indeterminado de tickets
 		listaTickets.add(ticket1);
@@ -50,11 +45,33 @@ public class SistemaTickets {
 		System.out.print("Ingrese numero de fila: ");
 		numFila = input.nextInt();
 		
+		Boolean bandera = false;
 		for (int i = 0; i < listaTickets.size(); i++) {
 			if(numFila == listaTickets.get(i).getFila()) {
-				System.out.println(listaTickets.get(i));
+				System.out.print(listaTickets.get(i));
+				bandera = true;
 			}
 		}
+		
+		System.out.println();
+		
+		if(!bandera) {
+			System.out.println("No hay informacion para esa fila");
+		}
+		
+		// Creacion de Clientes
+		Clientes cliente1 = new Clientes();
+		cliente1.setId(48);
+		cliente1.setNombre("roberto");
+		cliente1.setApellido("perez");
+		cliente1.getListaTickets().add(ticket3);
+		
+		Tickets ticket4 = new Tickets(4,2,3,new Date(),new Date(),3250);
+		cliente1.getListaTickets().add(ticket4);
+		
+		// Mostrar tickets asociados a un cliente
+		System.out.println();
+		System.out.print(cliente1.toString());
 		
 	}
 }
